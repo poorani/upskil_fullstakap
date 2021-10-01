2fbf2f17b02dd930fd2b8a3cd343488c919b25f5
poorani muthusamy <pooranimuthusamy@gmail.com>
2021-10-01 20:07:28 +0000

    Reset db for correct id and extracted plan params

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,9 @@
 class PagesController < ApplicationController
     def home
-        @weekly_plan = Plan.find(8)
-        @monthly_plan = Plan.find(9)
-        @weekend_plan = Plan.find(10)
-        @guest_plan = Plan.find(11)
+        @weekly_plan = Plan.find(1)
+        @monthly_plan = Plan.find(2)
+        @weekend_plan = Plan.find(3)
+        @guest_plan = Plan.find(4)
     end
     def about
     end
