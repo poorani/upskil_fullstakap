1b3bfe72591e01bf37e6779f1c69b4c41991291d
poorani muthusamy <pooranimuthusamy@gmail.com>
2021-10-01 18:48:44 +0000

    Added links and passed query string parameters for user signup links

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
     def home
+        @weekly_plan = Plan.find(8)
+        @monthly_plan = Plan.find(9)
+        @weekend_plan = Plan.find(10)
+        @guest_plan = Plan.find(11)
     end
     def about
     end
