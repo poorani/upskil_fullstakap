4f2535545b84bbab92c16bf32963c3beb382c595
poorani muthusamy <pooranimuthusamy@gmail.com>
2021-10-01 05:48:45 +0000

    Added Plan DB table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Plan.create(name: 'Weekly', price: 100 )
+Plan.create(name: 'Monthly', price: 340 )
+Plan.create(name: 'Weekend', price: 100 )
