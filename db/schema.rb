fad79f94e3c2ef8c20f708e832db8650802cd629
poorani muthusamy <pooranimuthusamy@gmail.com>
2021-10-05 20:40:17 +0000

    added profiles table, profiles model and association btn user and profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20211001163953) do
+ActiveRecord::Schema.define(version: 20211005202536) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
@@ -25,6 +25,18 @@ ActiveRecord::Schema.define(version: 20211001163953) do
     t.decimal  "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "plan_subscribed"
+    t.string   "phone_number"
+    t.string   "email"
+    t.string   "custom_notes"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   create_table "users", force: :cascade do |t|
