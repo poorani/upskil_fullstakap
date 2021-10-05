841bf838c099742359766f4dfd044f7a320c9a61
poorani muthusamy <pooranimuthusamy@gmail.com>
2021-10-05 20:02:22 +0000

    Added plan id check at registration controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+    
+  before_action :select_plan, only: :new
+  
   def create
     super do |resource|
       if params[:plan]
@@ -6,6 +9,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
         resource.save
         
       end
+    end
+  end
+  
+  private
+  def select_plan
+    unless (params[:plan] == '1' || params[:plan] =='2' || params[:plan] =='3'|| params[:plan] =='4' )
+      flash[:notice] = "Please select a membership plan to sign up."
+      redirect_to root_url
     end
   end
 end
