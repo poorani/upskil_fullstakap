d5cfb90e3db31405a66c3e371c001d10cc68e25a
poorani muthusamy <pooranimuthusamy@gmail.com>
2021-10-06 18:19:26 +0000

    added create action to save profile in db

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,4 +4,22 @@ class ProfilesController < ApplicationController
     # Render blank profile details form
     @profile = Profile.new
   end
+  
+  # POST to /users/:user_id/profile
+def create
+  # Ensure that we have the user who is filling out form
+  @user = User.find( params[:user_id] )
+  # Create profile linked to this specific user
+  @profile = @user.build_profile( profile_params )
+  if @profile.save
+    flash[:success] = "Profile updated!"
+    redirect_to root_path
+  else
+    render action: :new
+  end
+end
+private
+  def profile_params
+    params.require(:profile).permit(:first_name, :last_name, :plan_subscribed, :phone_number, :email, :custom_notes)
+  end
 end
